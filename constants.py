93d45a21f0a054ec49c2b785bb934899e13a271a
sh_hn <shideh.hashemian@gmail.com>
2020-12-28 12:34:19 +0330

    includes constant variables, like path to data's directory

---
+++ constants.py
@@ -0,0 +1,4 @@
+# set data_file_path value to the directory that your data are in
+data_file_path = 'D:\\University\\2020\\Fall 2020\\Topics in CS\\Assignments\\Final_project\\Data\\'
+document_file_path = 'documents\\'
+punctuations = '\\[!”#$%&’()*+,-./:";<=>?@[]^_`{|}~]'
