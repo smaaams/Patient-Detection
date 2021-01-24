188e95d9d1a6e0bba40bbc4c480bc500ce6943c0
sh_hn <shideh.hashemian@gmail.com>
2021-01-24 14:20:49 +0330

    made preprocessing.py more readable

--- preprocessing.py
+++ preprocessing.py
@@ -7,27 +7,51 @@ from constants import data_file_path
 
 
 def get_processed_data(csv_file_add):
+    """
+
+    :param csv_file_add: (string) path to were csv file is stored at (including it's filename)
+    :return: data: (dictionary) contains only required data for classification tasks
+    (which in the texts has been cleaned)
+
+    data dictionary structure:
+    {   'review': list of normalized reviews text (list of strings),
+        'condition':list of normalized conditions name (list of strings),
+         'drug': list of normalized drugs name (list of strings)
+    }
+     the reason for normalizing conditions and drugs name is that they might also be appeared in the reviews
+    """
+
+    # read csv file
     csv_data = pd.read_csv(csv_file_add)
 
+    # define a dictionary to store required data
     data = {'review': [], 'condition': [], 'drug': []}
 
     for index in range(csv_data.shape[0]):
+        # add normalized condition
         data['condition'].append(normalizer(str(csv_data['condition'][index])))
+        # add normalized drug name
         data['drug'].append(normalizer(str(csv_data['drugName'][index])))
+        # add normalized review
         data['review'].append(normalizer(str(csv_data['review'][index])))
 
     return data
 
 
 if __name__ == '__main__':
+    # define csv data paths
     train_add = os.path.join(data_file_path, 'drugsComTrain_raw.csv')
     test_add = os.path.join(data_file_path, 'drugsComTest_raw.csv')
+    # call get_preprocessed_data on training data
     train = get_processed_data(train_add)
+    # see the result of preprocessed training data
     print('--------------------------------     See Train Results    --------------------------------')
     for condition, drug, review in zip(train['condition'], train['drug'], train['review']):
         print('{}:  {}:     {}'.format(condition, drug, review))
 
+    # call get_preprocessed_data on test data
     test = get_processed_data(test_add)
+    # see the result of preprocessed test data
     print('--------------------------------     See Test Results    --------------------------------')
     for condition, drug, review in zip(test['condition'], test['drug'], test['review']):
         print('{}:  {}:     {}'.format(condition, drug, review))
